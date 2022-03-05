26b3d5f9bd4aca4b95a605262ed6bcdb3a4f0954
Cleianderson <cleianderson13@gmail.com>
2022-03-04 23:18:51 -0300

    fix: bug on notify warning

--- src/controllers/Warning.ts
+++ src/controllers/Warning.ts
@@ -9,24 +9,38 @@ export async function createWarning (req, res) {
   const newWarn = await Warning.create({ title, content, endDate })
   const allWarns = await Warning.find()
 
-  const dataOneSignal = {
+  let dataOneSignal = {
     app_id: '85b3451d-6f7d-481f-b66e-1f93fe069135',
     contents: { en: content, pt: content },
     heading: { en: title, pt: title },
-    included_segments: ['Dev'],
-    excluded_segments: ['All'],
     data: { allWarns }
   }
+
+  if(process.env.NODE_ENV === 'development') {
+    dataOneSignal = { 
+      ...dataOneSignal,
+      app_id: '9cf90441-8151-4e1a-91a5-ce90b102410c',
+      included_segments: ['Dev'],
+      excluded_segments: ['All'],
+    }
+  }
+
   const optionsOneSignal = {
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
-      Authorization: process.env.REST_API_KEY_ONE_SIGNAL
+      Authorization: `Basic ${process.env.REST_API_KEY_ONE_SIGNAL}`
     }
   }
-  const resolve = !testing
-    ? await axios.post('https://onesignal.com/api/v1/notifications', dataOneSignal, optionsOneSignal)
-    : {}
-  return res.status(200).json({ warning: newWarn, onesignal_response: resolve })
+  if(testing === false){
+    const resolve = await axios.post(
+      'https://onesignal.com/api/v1/notifications',
+       dataOneSignal,
+       optionsOneSignal,
+       )
+    return res.status(resolve.status).json({ warning: newWarn, onesignal_response: resolve.data })
+  } else {
+    return res.status(200).json({ ...dataOneSignal })
+  }
 }
 
 export async function getWarnings (req, res) {
